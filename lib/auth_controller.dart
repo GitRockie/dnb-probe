c7bc4d73172b73c0d52d7768cdc4a0d5e0c41c7a
GitRockie <elromankos@gmail.com>
2022-12-28 00:40:52 +0100

    GetX Authentication
    Firebase Auth and Core plugins install
    AuthController create
    Rx wraping <User?> model since we're using GetX
    Notifying _user with bindStream function
    Redirecting to Login Page if null user
    If logged => to Home Page
    ever takes a listener and callback Functuion _user changes notified
    Register function create Catch/Try Closure
    Dependency injection of AuthController on main

---
+++ lib/auth_controller.dart
@@ -0,0 +1,54 @@
+import 'dart:ui';
+
+import 'package:auth_app_flutter/home_page.dart';
+import 'package:auth_app_flutter/login_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class AuthController extends GetxController {
+  //Create globally accessible AuthController.instance..
+  static final AuthController instance = Get.find();
+  //Firebase User Model initializes here
+  late Rx<User?> _user;
+
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  @override
+  void onReady() {
+    super.onReady();
+    _user = Rx<User?>(auth.currentUser);
+    //Notifying _user with bindStream function
+    _user.bindStream(auth.userChanges());
+    ever(_user, _initialScreen);
+  }
+
+  _initialScreen(User? user) {
+    if (user == null) {
+      print('login page redirected');
+      Get.offAll(() => const LoginPage());
+    } else {
+      Get.offAll(() => const HomePage());
+    }
+  }
+
+  //Register function create
+  void register(String email, password) {
+    try {
+      auth.createUserWithEmailAndPassword(email: email, password: password);
+    } catch (e) {
+      Get.snackbar('About User', 'User message',
+          backgroundColor: Colors.redAccent,
+          snackPosition: SnackPosition.BOTTOM,
+          titleText: const Text(
+            '¡Vaya! Algo salió mal y la creación de la cuenta falló.',
+            style: TextStyle(color: Colors.white),
+          ),
+          messageText: Text(
+            e.toString(),
+            style: const TextStyle(color: Colors.white),
+          ));
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:auth_app_flutter/auth_controller.dart';
 import 'package:auth_app_flutter/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() {
+Future<void> main() async {
+  //Binding happens from Firebase
+  WidgetsFlutterBinding.ensureInitialized();
+  //Passing AuthController
+  await Firebase.initializeApp().then((value) => Get.put(AuthController()));
   runApp(const MyApp());
 }
 
@@ -17,15 +23,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const HomePage(),
