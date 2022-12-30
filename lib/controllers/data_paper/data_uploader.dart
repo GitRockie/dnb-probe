d82a3f525c9c22e102aa2e86033597d0b9ccc602
GitRockie <elromankos@gmail.com>
2022-12-30 01:09:40 +0100

    Reading the datos.txt

--- lib/controllers/data_paper/data_uploader.dart
+++ lib/controllers/data_paper/data_uploader.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 class DataUploader extends GetxController {
@@ -9,10 +10,16 @@ class DataUploader extends GetxController {
     final manifestContent = await DefaultAssetBundle.of(Get.context!)
         .loadString('AssetManifest.json');
     final Map<String, dynamic> manifestMap = json.decode(manifestContent);
+    //TODO:load txt and print path
     final papersInAssets = manifestMap.keys
         .where((path) =>
             path.startsWith('assets/DB/papers') && path.contains('.txt'))
         .toList();
-    print(papersInAssets);
+    //print(papersInAssets);
+    //Reading of the content
+    for (var paper in papersInAssets) {
+      String stringPaperContent = await rootBundle.loadString(paper);
+      print(stringPaperContent);
+    }
   }
 }
